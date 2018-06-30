de943816f08469cb897025b33a166ce3ae895bfa
Divya Kunnath <kunnathdivyal@gmail.com>
2018-06-30 23:39:10 +0800

    show 404 for requests with invalid code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def render_not_found_response(exception)
+	respond_to do |format|
+      format.html { render file: "#{Rails.root}/public/404", layout: false, status: :not_found }
+      format.json { render json: { message: 'Page not found' }, status: :not_found }
+	end
+  end
 end
