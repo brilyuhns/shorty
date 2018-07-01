2864a19c7449307e6764dc41b7c78c6b53edd659
Divya Kunnath <kunnathdivyal@gmail.com>
2018-07-01 14:38:58 +0800

    Add validations for short_url code

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,4 +1,6 @@
 class ShortUrl < ApplicationRecord
   validates :original_url, presence: true
   validates :original_url, url: true
+  validates :code, presence: true
+  validates :code, uniqueness: true
 end
