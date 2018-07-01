0086c394909b2514c125bf6db71a7e40ae059abb
Divya Kunnath <kunnathdivyal@gmail.com>
2018-07-01 12:36:30 +0800

    Spec changes for code

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -7,19 +7,10 @@ feature 'Home page' do
   # Scenario: Visit the home page
   #   Given I am a visitor
   #   When I visit the home page
-  #   Then I see "Welcome"
+  #   Then I see "URL to Shorten"
   scenario 'visit the home page' do
     visit root_path
     expect(page).to have_content 'Shorten URL'
     expect(page).to have_content 'URL to shorten'
-  end
-
-  scenario "shorten a url" do
-    visit "/short_urls/new"
-
-    fill_in "URL to shorten", :with => "http://www.google.com"
-    click_button "Shorten"
-    
-    expect(page).to have_text("Short url was successfully created.")
   end
 end
---
+++ spec/features/shorten_url_spec.rb
@@ -0,0 +1,21 @@
+# Feature: Shorten URL
+#   As a visitor
+#   I want to visit a home page
+#   So I can shorten a url
+feature 'Shorten New URL' do
+
+  # Scenario: Visit the short_urls/new
+  #   When I visit the short_urls/new
+  #   Then I see "URL to shorten"
+  scenario "shorten a url" do
+    visit "/short_urls/new"
+
+    fill_in "URL to shorten", :with => "http://www.google.com"
+    click_button "Shorten"
+    
+    expect(page).to have_text("Short url was successfully created.")
+    expect(page).to have_content(
+      "Shortened url: #{short_url_url(ShortUrl.last)}"
+    )
+  end
+end
